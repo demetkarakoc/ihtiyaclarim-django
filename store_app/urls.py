3827b2ef3d492938f4d7becfd7126fd6d0e7de35
Demet Krakoç <karakocdmet1881@gmail.com>
2024-01-03 19:58:52 +0300

    first commit

---
+++ store_app/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.store_app, name ="store_app"),
+    path('cart/', views.cart, name ="cart"),
+    path('checkout/', views.checkout, name ="checkout"),
+
+    path('update_item/', views.updateItem, name ="update_item"),
+    path('process_order/', views.processOrder, name ="update_item"),
+
+]
